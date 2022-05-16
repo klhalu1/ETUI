142b246464d437797b458f12539cb8cd157784fa
CHEN SU <1329508445@qq.com>
2022-05-16 11:13:25 +0800

    缓存数据服务器
    添加缓存数据服务器逻辑UnitCache

---
+++ Server/Model/Demo/UnitCache/UnitCacheComponent.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace ET
+{
+    [ChildType(typeof(UnitCache))]
+    public class UnitCacheComponent : Entity,IAwake,IDestroy
+    {
+        public Dictionary<string, UnitCache> UnitCaches = new Dictionary<string, UnitCache>();
+
+        public List<string> UnitCacheKeyList = new List<string>();
+    }
+}
